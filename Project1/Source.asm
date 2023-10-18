2d963bb395fe58a4319de7c8fc4deab46a75516c
B6 <jiaheng921122@gmail.com>
2023-10-18 16:17:14 +0800

    hw2

--- Project1/Source.asm
+++ Project1/Source.asm
@@ -1,52 +1,31 @@
 include Irvine32.inc
 
 .data
-    myID byte "111502555"  ; Team leader student ID
-    myID2 byte "111502554" ; Team member student ID
-    size_ID = 9    ; size_ID indicates the length of myID
-    result byte 9 DUP(?)  ; save the result
-
+    MyID DWORD 0
 .code
+
+fact PROC
+start:
+    cmp ebx, 1
+    jz e
+    jb e ;MyID == 0
+    jmp rec
+rec:
+    mov edx, ebx
+    dec ebx
+    mul ebx
+    
+    call fact
+    jmp start 
+e:
+    ret
+fact ENDP
+
 Main PROC
-    mov esi, offset myID
-    mov ebx, offset myID2
-    mov edi, offset result
-    mov ecx, size_ID
-
-L1: 
-    mov al, [esi]
-    mov ah, [ebx]
-    inc esi
-    inc ebx
-    cmp al, ah
-    jz LA
-    ja LB
-    jb LC
-    loop L1
-
-LA:
-    mov ax, 'A'
-    mov [edi], ax
-    inc edi
-    loop L1
-    jmp ED
-
-LB:
-    mov ax, 'B'
-    mov [edi], ax
-    inc edi
-    loop L1
-    jmp ED
-
-LC:
-    mov ax, 'C'
-    mov [edi], ax
-    inc edi
-    loop L1
-    jmp ED
-
-ED:         ;to leave the loop
-    exit    
-
+    mov ebx, MyID
+    mov eax, ebx
+    call fact
+    
+    exit
 main ENDP
 END main
